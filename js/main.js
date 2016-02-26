08f3af596ce34bf205ff9cce01485981a15a15fb
Amos Marsters <amosmarsters@Amoss-MacBook-Pro.local>
2016-02-26 16:35:15 -0500

    if statements

--- js/main.js
+++ js/main.js
@@ -8,15 +8,17 @@ var adjective = prompt('An adjective (beautiful, super, etc.)');
 
 var verb = prompt('A past tense verb (ran, burped, etc.)');
 
-var story = prompt('Which story would you like?' + '<br>' + 'a) Snacking' + '<br>' +
-'b) Escaping' + '<br>' + 'c) Winning' + '<br>' + 'Enter the letter below:');
+var story = prompt('Which story would you like?' + '\n' + 'a) Snacking' + '\n' +
+'b) Escaping' + '\n' + 'c) Winning' + '\n' + 'Enter the letter below:');
 
-// if (a) {
+if (story === 'a') {
+  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verb + ' for hours.');
+}
 
-// }
+if (story === 'b') {
+ document.write('Amelia ' + verb + ' through the ' + adjective + colour + ' nebula escaping the space ' + creature + '.');
+}
 
-// // After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.
-
-// // Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
-
-// // Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
+if (story === 'c') {
+ document.write('Jackson chose his ' + adjective + ', ' + creature + ' card and ' + verb + ' it to the table knowing he won.');
+}
