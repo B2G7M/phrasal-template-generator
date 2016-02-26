7a78f0175fdb87d3399c48011d3165bfb9c13184
Amos Marsters <amosmarsters@Amoss-MacBook-Pro.local>
2016-02-26 15:36:29 -0500

    added content and boiler plate

---
+++ js/main.js
@@ -0,0 +1,22 @@
+alert('Let’s make a story together!');
+
+var colour = prompt('A colour (orange, indigo, etc.)');
+
+var creature = prompt('A creature (dragon, moth, etc.)');
+
+var adjective = prompt('An adjective (beautiful, super, etc.)');
+
+var verb = prompt('A past tense verb (ran, burped, etc.)');
+
+var story = prompt('Which story would you like?' + '<br>' + 'a) Snacking' + '<br>' +
+'b) Escaping' + '<br>' + 'c) Winning' + '<br>' + 'Enter the letter below:');
+
+// if (a) {
+
+// }
+
+// // After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.
+
+// // Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
+
+// // Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
